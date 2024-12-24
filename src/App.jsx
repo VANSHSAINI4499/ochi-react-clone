5f4f9c7ffc8ec7ed7f8bd7343255cc49c0dc1b1b
Vansh <vanshsaini4499@gmail.com>
2024-12-24 22:18:09 +0530

    landing page pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
-    <div>
+    <div className='w-full h-screen text-white'>
+      <Navbar/>
+      <LandingPage/>
       
     </div>
   )
